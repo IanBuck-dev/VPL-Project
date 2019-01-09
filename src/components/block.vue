8fd336078b48e8ff5076b3d97ced354817a4f5c7
Ian Junior <buckjunior@live.de>
2019-01-09 14:56:07 +0100

    09.01.19 Changes

---
+++ src/components/block.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="movable">{{block.name}}</div>
+</template>
+
+<script>
+export default {
+  props: ['block']
+}
+</script>
+
+<style>
+.movable {
+  font-size: 25px;
+  color: #fff;
+  display: inline-block;
+  padding: 0px;
+  position: absolute;
+  border: 2px solid rgb(46, 45, 45);
+  border-radius: 5px;
+}
+</style>
