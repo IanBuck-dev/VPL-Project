4f11088d64e2702905d40d805864d3d25e373cf7
Ian Junior <buckjunior@live.de>
2019-01-10 13:19:14 +0100

    Drag and Drop + Collision Detection

--- src/pages/EditorPage.vue
+++ src/pages/EditorPage.vue
@@ -43,20 +43,53 @@ export default {
           x: 300,
           y: 200
         }
-      ]
+      ],
+      removeBlockTimeout: false
     }
   },
   methods: {
+    detectCollisions: function (block, index) {
+      var movingblock = this.blocks[index - 1]
+
+      var touching = (
+        (block.x + 50 > movingblock.x - 50 && movingblock.y + 50 > block.y - 50) && (block.x - 50 < movingblock.x + 50 && movingblock.y - 50 < block.y + 50) && !(block.id === movingblock.id)
+      )
+
+      if (touching) {
+        movingblock.x = block.x + 100
+        movingblock.y = block.y
+        this.moving = false
+      }
+      console.log(touching)
+    },
+    isTouching: function (block, index) {
+      var movingblock = this.blocks[index - 1]
+      var touching = (
+        (block.x + 50 > movingblock.x - 50 && movingblock.y + 50 > block.y - 50) && (block.x - 50 < movingblock.x + 50 && movingblock.y - 50 < block.y + 50) && !(block.id === movingblock.id)
+      )
+
+      if (touching) {
+        this.removeBlockTimeout = true
+        setTimeout(function () {
+          this.removeBlockTimeout = false
+        }, 500)
+      }
+    },
     moveStart: function (event, index) {
       this.moving = true
-      console.log(event)
-      console.log(index)
+
+      this.blocks.forEach(element => {
+        this.isTouching(element, index)
+      })
     },
     moveActive: function (event, index) {
-      if (this.moving) {
+      if (this.moving && !this.removeBlockTimeout) {
         console.log(event)
         this.blocks[index - 1].x = event.x - 50
         this.blocks[index - 1].y = event.y - 100
+        this.blocks.forEach(element => {
+          this.detectCollisions(element, index)
+        })
       }
     },
     moveEnd: function (event, index) {
